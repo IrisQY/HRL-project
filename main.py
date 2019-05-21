3f27713c4f52e784bd4c682b9b082b52052af2f8
IrisQY <iriswangsu@gmail.com>
2019-05-21 12:59:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from experiment import DQN, EnsembleDQN, BootDQN
 import argparse
 # python main.py --mode BootDQN --num_episodes 2000 --max_timesteps 1000
+# python main.py --mode DQN --num_episodes 2000 --max_timesteps 1000
+# python main.py --mode EnsembleDQN --num_episodes 2000 --max_timesteps 1000
 
 parser = argparse.ArgumentParser()
 
